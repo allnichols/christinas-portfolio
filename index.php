fbeb70378640be0423139e8732915270cbaa9ebd
allnichols <allenmaxnichols@gmail.com>
2019-08-27 21:06:41 -0500

    corrected above the fold images position

--- index.php
+++ index.php
@@ -4,14 +4,20 @@
 
 <section class="above-the-fold">
 
-<img src="<?php the_field('main-image') ?>" alt="Christina Q Nichols">
+<div class="image-container" style="background-image: url(<?php the_field('main-image') ?>)">
+</div>
 
+
+<div class="above-the-fold_text">
+<h1>Christina Q. Nichols</h1>
 <nav>
     <a href="#!">CopyWriter</a>
     <a href="#!">Photographer</a>
     <a href="#!">Video Editor</a>
-    <a href="#!"></a>
+    <!-- <a href="#!"></a> -->
 </nav>
+</div>
+
 
 </section>
 
