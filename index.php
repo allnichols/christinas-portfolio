4bfdbbf9aebeabc67d13d381f732513a7020337e
allnichols <allenmaxnichols@gmail.com>
2019-08-29 20:45:31 -0500

    Above the fold text positioning changes.

--- index.php
+++ index.php
@@ -4,19 +4,26 @@
 
 <section class="above-the-fold">
 
-<div class="image-container" style="background-image: url(<?php the_field('main-image') ?>)">
+<div class="above-the-fold-container">
+    <div class="image-container" style="background-image: url(<?php the_field('main-image') ?>)">
+
+    <div class="above-the-fold_text">
+        <h1>Christina Q. Nichols</h1>
+        <nav>
+            <a href="#!">CopyWriter</a>
+            <a href="#!">Photographer</a>
+            <a href="#!">Video Editor</a>
+            <!-- <a href="#!"></a> -->
+        </nav>
+    </div>
+    </div>
+
+    
+
 </div>
+ 
 
 
-<div class="above-the-fold_text">
-<h1>Christina Q. Nichols</h1>
-<nav>
-    <a href="#!">CopyWriter</a>
-    <a href="#!">Photographer</a>
-    <a href="#!">Video Editor</a>
-    <!-- <a href="#!"></a> -->
-</nav>
-</div>
 
 
 </section>
